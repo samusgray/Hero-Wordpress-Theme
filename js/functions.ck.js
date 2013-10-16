a672599e214db47d5d6a230feadff1cc24de116b
Aaron Gray <aaron@hellometa.com>
2013-10-16 10:43:22 -0600

    Reformat Scss architecture and include responsive navigation from Underscore Theme

--- js/functions.ck.js
+++ js/functions.ck.js
@@ -1,1 +1,8 @@
-;
+// jQuery(function($) {
+//   $test = $('.site-title')
+//   alert($test);
+// });
+/**
+ *
+ * Handles toggling the navigation menu for small screens.
+ */(function(){var e,t,n;e=document.getElementById("site-navigation");if(!e)return;t=e.getElementsByTagName("h1")[0];if("undefined"==typeof t)return;n=e.getElementsByTagName("ul")[0];if("undefined"==typeof n){t.style.display="none";return}-1===n.className.indexOf("nav-menu")&&(n.className+=" nav-menu");t.onclick=function(){-1!==e.className.indexOf("toggled")?e.className=e.className.replace(" toggled",""):e.className+=" toggled"}})();
---
+++ styleguide/page-styleguide.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: StyleGuide
+ *
+ * Description: A page template that provides a key component of WordPress as a CMS
+ * by meeting the need for a carefully crafted introductory page. The front page template
+ * in Twenty Twelve consists of a page content area for adding text, images, video --
+ * anything you'd like -- followed by front-page-only widgets in one or two columns.
+ */
+
+ get_header(); ?>
+
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'content', 'page' ); ?>
+      <?php endwhile; // end of the loop. ?>
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
