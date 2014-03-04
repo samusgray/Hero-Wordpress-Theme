da5ba2a15e9e318015f9414414256148be6e6dc7
Aaron Gray <aaron@hellometa.com>
2014-03-04 11:39:04 -0700

    Include default typography markup

---
+++ components/typography/_typography.php
@@ -0,0 +1,34 @@
+<h1>Section Header</h1>
+<h2>Section Header</h2>
+<h3>Section Header</h3>
+<h4>Section Header</h4>
+<h5>Section Header</h5>
+
+<p>Lorem ipsum dolor sit amet, <strong>strong, bolded text</strong> consectetur adipisicing elit. Ex, quos, quis, laboriosam ullam ipsam illum repellat temporibus reiciendis animi quae veniam eaque tenetur odio quod soluta! Dolor esse modi velit.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, provident, 
+  commodi, suscipit facere maxime quod dolore accusamus ipsa reprehenderit 
+  placeat quibusdam natus cumque nulla perferendis eveniet qui autem eius ipsam. 
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, provident, 
+  commodi, suscipit facere maxime quod dolore accusamus ipsa reprehenderit placeat 
+  quibusdam natus cumque nulla perferendis eveniet qui autem eius ipsam.</p>
+
+  <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, 
+    aliquam, corporis odit error vero recusandae rem quod omnis aspernatur dolorem 
+    maxime quos quibusdam perferendis quam labore velit architecto culpa tempore.
+  </blockquote>
+
+  <ul>
+    <li>Unordered Item</li>
+    <li>Unordered Item</li>
+    <li>Unordered Item</li>
+    <li>Unordered Item</li>
+    <li>Unordered Item</li>
+  </ul>
+
+  <ol>
+    <li>Ordered Item</li>
+    <li>Ordered Item</li>
+    <li>Ordered Item</li>
+    <li>Ordered Item</li>
+    <li>Ordered Item</li>
+  </ol>
